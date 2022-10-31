8fe3c6ab4dbaef444613908ab309db30d7ae405c
Anzhelika Karpik <karpik.lika@mail.ru>
2022-10-31 22:40:55 +0300

    Add search for items

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
   const [basketOpened, setBasketOpened] = useState(false);
   const [basketItems, setBasketItems] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
   const openBasket = () => {
     setBasketOpened(true);
@@ -24,6 +25,10 @@ function App() {
     setBasketItems((prev) => prev.filter((item) => Number(item.id) !== Number(id)));
   }
 
+  const onChangeInput = (e) => {
+    setSearchValue(e.target.value);
+  }
+
   return (
     <div className="App">
       {basketOpened && <Basket onCloseBasket={() => closeBasket()} 
@@ -31,10 +36,20 @@ function App() {
                                 onDeleteItem={onDeleteItem}/>}
       <Header onClickCart={() => openBasket()} />
       <div className="body">
-        <div className="label_all_goods">Все кроссовки</div>
+        <div className="all_goods">
+          {searchValue ? <div className="all_goods_label">Поиск по запросу: {searchValue}</div> 
+                      : <div className="all_goods_label">Все кроссовки</div>}
+          <div className="all_goods_search">
+            <img className="all_goods_img_search" src="/img/search.svg" alt="Search" />
+            {searchValue && <img className="all_goods_img_delete" src="/img/close.png" alt="Delete" onClick={() => setSearchValue('')}/>}
+            <input type="text" onChange={onChangeInput} value={searchValue} placeholder="Поиск..."/>
+          </div>
+        </div>
         <div className="goods">
           {
-            goods.map((item, i) => <Card key={item.id} id={i} cost={item.cost} title={item.title} alt={item.alt} img={item.img} 
+            goods
+            .filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+            .map((item, i) => <Card key={item.id} id={i} cost={item.cost} title={item.title} alt={item.alt} img={item.img} 
                                     basketItems={basketItems} setBasketItems={setBasketItems}
                                     onDeleteItem={onDeleteItem}
                               />)
